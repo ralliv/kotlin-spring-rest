4f2c12baa0d525aac2dcfd50c30c3cc842fccc7d
Adrián Villar <adrian.villargomez@telefonica.com>
2023-03-03 12:43:48 +0100

    kotlin spring rest init

---
+++ src/main/kotlin/com/ralliv/kotlinspringrest/KotlinSpringRestApplication.kt
@@ -0,0 +1,11 @@
+package com.ralliv.kotlinspringrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringRestApplication>(*args)
+}
---
+++ src/test/kotlin/com/ralliv/kotlinspringrest/KotlinSpringRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ralliv.kotlinspringrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
